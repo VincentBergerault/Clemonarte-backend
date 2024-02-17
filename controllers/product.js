1f090aedab0d60952fa4fa2f53992c9cdcd0ef92
VincentBergerault <VincentBergerault>
2024-02-17 20:08:24 +0100

    setup server

---
+++ controllers/product.js
@@ -0,0 +1,2 @@
+const express = require("express");
+const Product = require("../models/product.model");
--- models/product.model.ts
+++ models/product.model.ts
@@ -1,5 +1,4 @@
 import { Schema, model } from "mongoose";
-// import bcrypt from "mongoose-bcrypt";
 
 const ProductSchema = new Schema({
   name: { type: String, unique: true },
@@ -8,4 +7,3 @@ const ProductSchema = new Schema({
 });
 
 export const Product = model("Product", ProductSchema);
-// https://medium.com/@flanker72/nuxt3-complex-solutions-database-integration-8df941f0fb82
