6ae66f2dbbb8c7aac058f91efc06c039197c2297
VincentBergerault <bergerault.vb@gmail.com>
2024-02-17 18:42:27 +0100

    init

---
+++ server/api/product/index.get.ts
@@ -0,0 +1,8 @@
+import { Product } from "~/server/models/product.model";
+
+export default defineEventHandler(async () => {
+  const filter = {};
+  const products = await Product.find(filter);
+
+  return { products };
+});
